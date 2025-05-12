a0f682d63fc7eb963ca0fd33a2f8e4f6f2d566cc
Maurízio Dellano <dellano.liagi2004@gmail.com>
2025-05-12 20:01:56 -0300

    first commit

---
+++ gestaoEnergia/src/main/java/br/com/fiap/gestaoEnergia/GestaoEnergiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gestaoEnergia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoEnergiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoEnergiaApplication.class, args);
+	}
+
+}
---
+++ gestaoEnergia/src/test/java/br/com/fiap/gestaoEnergia/GestaoEnergiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gestaoEnergia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEnergiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
